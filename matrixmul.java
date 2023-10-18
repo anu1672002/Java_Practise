c5690ad9437caec5bb55a70e469a475d6a5aa5b3
anu1672002 <141577696+anu1672002@users.noreply.github.com>
2023-10-18 19:56:57 +0530

    Add files via upload

--- matrixmul.java
+++ matrixmul.java
@@ -1,1 +1,26 @@
+package Anuex;
 
+public class matrixmul {
+
+	public static void main(String[] args) {
+		int a[][] = {{1,2},{3,4}};
+		int b[][] = {{1,2},{3,4}};
+		int c[][] = new int[a.length][b.length];
+		
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<b.length;j++) {
+				c[i][j] = 0;
+				for (int k = 0; k < a[0].length; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+				}
+			}
+		}
+		for(int i=0;i<a.length;i++) {
+			for(int j=0;j<b.length;j++) {
+				System.out.println(c[i][j]);
+			}
+		}
+		
+	}
+
+}
